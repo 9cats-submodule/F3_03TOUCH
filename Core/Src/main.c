6203265de2e51f74b9ca5360d714d23c3985506d
HuangYi <123337671@qq.com>
2021-05-17 14:06:22 +0800

    fix : 修改编码与可能出现的警告

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,29 +51,29 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-//娓呯┖灞忓箷骞跺湪鍙充笂瑙掓樉绀?"RST"
+//清空屏幕并在右上角显??"RST"
 void Load_Drow_Dialog(void)
 {
-	LCD_Clear(WHITE);//娓呭睆   
- 	POINT_COLOR=BLUE;//璁剧疆瀛椾綋涓鸿摑鑹? 
-	LCD_ShowString(lcddev.width-24,0,200,16,16,"RST");//鏄剧ず娓呭睆鍖哄煙
-  POINT_COLOR=RED;//璁剧疆鐢荤瑪钃濊壊 
+	LCD_Clear(WHITE);//清屏   
+ 	POINT_COLOR=BLUE;//设置字体为蓝?? 
+	LCD_ShowString(lcddev.width-24,0,200,16,16,(u8 *)"RST");//显示清屏区域
+  POINT_COLOR=RED;//设置画笔蓝色 
 }
 ////////////////////////////////////////////////////////////////////////////////
-//鐢靛瑙︽懜灞忎笓鏈夐儴鍒?
-//鐢绘按骞崇嚎
-//x0,y0:鍧愭爣
-//len:绾块暱搴?
-//color:棰滆壊
+//电容触摸屏专有部??
+//画水平线
+//x0,y0:坐标
+//len:线长??
+//color:颜色
 void gui_draw_hline(u16 x0,u16 y0,u16 len,u16 color)
 {
 	if(len==0)return;
 	LCD_Fill(x0,y0,x0+len-1,y0,color);	
 }
-//鐢诲疄蹇冨渾
-//x0,y0:鍧愭爣
-//r:鍗婂緞
-//color:棰滆壊
+//画实心圆
+//x0,y0:坐标
+//r:半径
+//color:颜色
 void gui_fill_circle(u16 x0,u16 y0,u16 r,u16 color)
 {											  
 	u32 i;
@@ -97,39 +97,39 @@ void gui_fill_circle(u16 x0,u16 y0,u16 r,u16 color)
 		gui_draw_hline(x0-x,y0-i,2*x,color);
 	}
 }  
-//涓や釜鏁颁箣宸殑缁濆鍊? 
-//x1,x2锛氶渶鍙栧樊鍊肩殑涓や釜鏁?
-//杩斿洖鍊硷細|x1-x2|
+//两个数之差的绝对?? 
+//x1,x2：需取差值的两个??
+//返回值：|x1-x2|
 u16 my_abs(u16 x1,u16 x2)
 {			 
 	if(x1>x2)return x1-x2;
 	else return x2-x1;
 }  
-//鐢讳竴鏉＄矖绾?
-//(x1,y1),(x2,y2):绾挎潯鐨勮捣濮嬪潗鏍?
-//size锛氱嚎鏉＄殑绮楃粏绋嬪害
-//color锛氱嚎鏉＄殑棰滆壊
+//画一条粗??
+//(x1,y1),(x2,y2):线条的起始坐??
+//size：线条的粗细程度
+//color：线条的颜色
 void lcd_draw_bline(u16 x1, u16 y1, u16 x2, u16 y2,u8 size,u16 color)
 {
 	u16 t; 
 	int xerr=0,yerr=0,delta_x,delta_y,distance; 
 	int incx,incy,uRow,uCol; 
 	if(x1<size|| x2<size||y1<size|| y2<size)return; 
-	delta_x=x2-x1; //璁＄畻鍧愭爣澧為噺 
+	delta_x=x2-x1; //计算坐标增量 
 	delta_y=y2-y1; 
 	uRow=x1; 
 	uCol=y1; 
-	if(delta_x>0)incx=1; //璁剧疆鍗曟鏂瑰悜 
-	else if(delta_x==0)incx=0;//鍨傜洿绾? 
+	if(delta_x>0)incx=1; //设置单步方向 
+	else if(delta_x==0)incx=0;//垂直?? 
 	else {incx=-1;delta_x=-delta_x;} 
 	if(delta_y>0)incy=1; 
-	else if(delta_y==0)incy=0;//姘村钩绾? 
+	else if(delta_y==0)incy=0;//水平?? 
 	else{incy=-1;delta_y=-delta_y;} 
-	if( delta_x>delta_y)distance=delta_x; //閫夊彇鍩烘湰澧為噺鍧愭爣杞? 
+	if( delta_x>delta_y)distance=delta_x; //选取基本增量坐标?? 
 	else distance=delta_y; 
-	for(t=0;t<=distance+1;t++ )//鐢荤嚎杈撳嚭 
+	for(t=0;t<=distance+1;t++ )//画线输出 
 	{  
-		gui_fill_circle(uRow,uCol,size,color);//鐢荤偣 
+		gui_fill_circle(uRow,uCol,size,color);//画点 
 		xerr+=delta_x ; 
 		yerr+=delta_y ; 
 		if(xerr>distance) 
@@ -145,9 +145,9 @@ void lcd_draw_bline(u16 x1, u16 y1, u16 x2, u16 y2,u8 size,u16 color)
 	}  
 }   
 ////////////////////////////////////////////////////////////////////////////////
-//5涓Е鎺х偣鐨勯鑹?(鐢靛瑙︽懜灞忕敤)												 
+//5个触控点的颜??(电容触摸屏用)												 
 const u16 POINT_COLOR_TBL[5]={RED,GREEN,BLUE,BROWN,GRED};  
-//鐢甸樆瑙︽懜灞忔祴璇曞嚱鏁?
+//电阻触摸屏测试函??
 void rtp_test(void)
 {
 	u8 key;
@@ -156,18 +156,18 @@ void rtp_test(void)
 	{
 	 	key=KEY_Scan(0);
 		tp_dev.scan(0); 		 
-		if(tp_dev.sta&TP_PRES_DOWN)			//瑙︽懜灞忚鎸変笅
+		if(tp_dev.sta&TP_PRES_DOWN)			//触摸屏被按下
 		{	
 		 	if(tp_dev.x[0]<lcddev.width&&tp_dev.y[0]<lcddev.height)
 			{	
-				if(tp_dev.x[0]>(lcddev.width-24)&&tp_dev.y[0]<16)Load_Drow_Dialog();//娓呴櫎
-				else TP_Draw_Big_Point(tp_dev.x[0],tp_dev.y[0],RED);		//鐢诲浘	  			   
+				if(tp_dev.x[0]>(lcddev.width-24)&&tp_dev.y[0]<16)Load_Drow_Dialog();//清除
+				else TP_Draw_Big_Point(tp_dev.x[0],tp_dev.y[0],RED);		//画图	  			   
 			}
-		}else delay_ms(10);	//娌℃湁鎸夐敭鎸変笅鐨勬椂鍊? 	    
-		if(key==KEY0_PRES)	//KEY0鎸変笅,鍒欐墽琛屾牎鍑嗙▼搴?
+		}else delay_ms(10);	//没有按键按下的时?? 	    
+		if(key==KEY0_PRES)	//KEY0按下,则执行校准程??
 		{
-			LCD_Clear(WHITE);	//娓呭睆
-		    TP_Adjust();  		//灞忓箷鏍″噯 
+			LCD_Clear(WHITE);	//清屏
+		    TP_Adjust();  		//屏幕校准 
 			TP_Save_Adjdata();	 
 			Load_Drow_Dialog();
 		}
@@ -175,12 +175,12 @@ void rtp_test(void)
 		if(i%20==0)LED0_T;
 	}
 }
-//鐢靛瑙︽懜灞忔祴璇曞嚱鏁?
+//电容触摸屏测试函??
 void ctp_test(void)
 {
 	u8 t=0;
 	u8 i=0;	  	    
- 	u16 lastpos[5][2];		//鏈?鍚庝竴娆＄殑鏁版嵁 
+ 	u16 lastpos[5][2];		//??后一次的数据 
 	while(1)
 	{
 		tp_dev.scan(0);
@@ -188,7 +188,7 @@ void ctp_test(void)
 		{
 			if((tp_dev.sta)&(1<<t))
 			{
-                //printf("X鍧愭爣:%d,Y鍧愭爣:%d\r\n",tp_dev.x[0],tp_dev.y[0]);
+                //printf("X坐标:%d,Y坐标:%d\r\n",tp_dev.x[0],tp_dev.y[0]);
 				if(tp_dev.x[t]<lcddev.width&&tp_dev.y[t]<lcddev.height)
 				{
 					if(lastpos[t][0]==0XFFFF)
@@ -197,12 +197,12 @@ void ctp_test(void)
 						lastpos[t][1] = tp_dev.y[t];
 					}
                     
-					lcd_draw_bline(lastpos[t][0],lastpos[t][1],tp_dev.x[t],tp_dev.y[t],2,POINT_COLOR_TBL[t]);//鐢荤嚎
+					lcd_draw_bline(lastpos[t][0],lastpos[t][1],tp_dev.x[t],tp_dev.y[t],2,POINT_COLOR_TBL[t]);//画线
 					lastpos[t][0]=tp_dev.x[t];
 					lastpos[t][1]=tp_dev.y[t];
 					if(tp_dev.x[t]>(lcddev.width-24)&&tp_dev.y[t]<20)
 					{
-						Load_Drow_Dialog();//娓呴櫎
+						Load_Drow_Dialog();//清除
 					}
 				}
 			}else lastpos[t][0]=0XFFFF;
@@ -227,7 +227,7 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
   u8 x=0;
-  u8 lcd_id[12];						//瀛樻斁LCD ID瀛楃涓?
+  u8 lcd_id[12];						//存放LCD ID字符??
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -251,22 +251,22 @@ int main(void)
   /* USER CODE BEGIN 2 */
   delay_init(72);
   LCD_Init();
-	tp_dev.init();				   		//瑙︽懜灞忓垵濮嬪寲 
+	tp_dev.init();				   		//触摸屏初始化 
 	
 	POINT_COLOR=RED;
-	LCD_ShowString(30,50,200,16,16,"Mini STM32");	
-	LCD_ShowString(30,70,200,16,16,"TOUCH TEST");	
-	LCD_ShowString(30,90,200,16,16,"ATOM@ALIENTEK");
-	LCD_ShowString(30,110,200,16,16,"2019/11/15");	 		
+	LCD_ShowString(30,50,200,16,16,(u8 *)"Mini STM32");
+	LCD_ShowString(30,70,200,16,16,(u8 *)"TOUCH TEST");
+	LCD_ShowString(30,90,200,16,16,(u8 *)"ATOM@ALIENTEK");
+	LCD_ShowString(30,110,200,16,16,(u8 *)"2019/11/15");
   if(tp_dev.touchtype!=0XFF)
 	{
-		LCD_ShowString(30,130,200,16,16,"Press KEY0 to Adjust");//鐢甸樆灞忔墠鏄剧ず
+		LCD_ShowString(30,130,200,16,16,(u8 *)"Press KEY0 to Adjust");//电阻屏才显示
 	}
 	delay_ms(1500);
  	Load_Drow_Dialog();	 	
 	
-	if(tp_dev.touchtype&0X80)ctp_test();//鐢靛灞忔祴璇?
-	else rtp_test(); 					//鐢甸樆灞忔祴璇?  
+	if(tp_dev.touchtype&0X80)ctp_test();//电容屏测??
+	else rtp_test(); 					//电阻屏测??  
   /* USER CODE END 2 */
 
   /* Infinite loop */
